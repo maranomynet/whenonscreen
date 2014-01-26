a994b4e967aaeadd595ff0c8bdfcc3d33e33be29
mar <mar.nospam@anomy.net>
2014-01-26 16:57:33 +0000

    major overhaul - bugfixes and added support for dynamic range values - either as percentage strings, or as custom functions.

--- 1.0/jquery.whenonscreen-1.0.js
+++ 1.0/jquery.whenonscreen-1.0.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------------
 // jQuery.fn.whenOnScreen v 1.0
 // ----------------------------------------------------------------------------------
-// (c) 2010-2013 Hugsmiðjan ehf  -- http://www.hugsmidjan.is
+// (c) 2010-2014 Hugsmiðjan ehf  -- http://www.hugsmidjan.is
 //  written by:
 //   * Már Örlygsson        -- http://mar.anomy.net
 //
@@ -39,13 +39,21 @@
 //                     b) { top:-100, bottom:-300, left:0, right:40,   customElm:[Object] },
 //                     c) { radius:50, top:50, bottom:50, left:50, right:50  },
 //      * scrTop      Number - Current window/viewport boundries in pixels
+//        srcHeight   ...
 //        scrBottom   ...
-//        scrLeft     ...
-//        scrRight    ...
+//        scrLeft     ... (if leftright is enabled)
+//        srcWidth    ... (if leftright is enabled)
+//        scrRight    ... (if leftright is enabled)
 //      * elmTop      Number -  Current element boundries in pixels
+//        elmHeight   ...
 //        elmBottom   ...
-//        elmLeft     ...
-//        elmRight    ...
+//        elmLeft     ... (if leftright is enabled)
+//        elmWidth    ... (if leftright is enabled)
+//        elmRight    ... (if leftright is enabled)
+//      * isElmBelow  Boolean - is the element outside (below|above|right of|left of) the viewport
+//        isElmAbove  ...
+//        isElmRight  ...
+//        isElmLeft   ...
 //      * leftright   Boolean - is horizontal boundry checking enabled for this element
 //      * live        Boolean - is element position + size recalculated every time?
 //
@@ -57,8 +65,10 @@
 //    $.whenOnScreen.throttle = 200;
 //    // Indicate that left/right boundries should also be checked by default (default: false)
 //    $.whenOnScreen.leftright = true;
-//    // Set a default range (default: 50 --> [{ radius:50 }] )
-//    $.whenOnScreen.ranges = [{ radius:100 }];
+//    // Set the list of default range objects (default: `[{ radius:50 }]` )
+//    $.whenOnScreen.ranges = [ { radius:100, top: -50 } ];
+//    // Disable caching of element offsets and dimensions.
+//    $.whenOnScreen.live = true;
 //
 //
 //    // Configuration:  =======================================================
@@ -69,14 +79,33 @@
 //    // Indicate that for this set of elements left/right boundries should also be checked
 //    sections.whenOnScreen({ leftright:true });
 //
-//    // Ask that size/position for these elements should be measured on every scroll/window.resize event (default: false)
+//    // Ask that size/position for these elements should be measured
+//    // on every scroll/window.resize event (default: false)
 //    sections.whenOnScreen({ live:true });
 //
 //    // single 'range' with radius of 100px (offscreen)
-//    sections.whenOnScreen( 100 );
+//    sections.whenOnScreen({ ranges:100 });
 //
 //    // single 'range' with radius of 100px (offscreen)
 //    sections.whenOnScreen({ ranges:[{ radius:100 }] });
+//
+//    // single 'range' with radius of 25% af viewport size
+//    // (counts as onscreen when edge of the element has entered 25% of screen dimension)
+//    sections.whenOnScreen({ ranges: '-25%s' });
+//
+//    // single 'range' with radius of 25% af element size
+//    // (counts as onscreen when more than 25% of the element is visible)
+//    sections.whenOnScreen({ ranges: '-25%e' });
+//
+//    // single 'range' calculated per element by a dynamic function.
+//    // (The function's return value is cached unless "live" flag is true)
+//    sections.whenOnScreen({
+//        ranges: function(sizes, side){
+//            // same as the shorthand '-25%e' above
+//            var dimension = /Top|Bottom/.test(side) ? 'Height' : 'Width';
+//            return: -.25 * sizes['elm'+dimension];
+//          }
+//    });
 //
 //    // single 'range' with varying radii
 //    sections.whenOnScreen({ ranges:[{ top:100, bottom:-100, left:50, right:-50 }] });
@@ -115,102 +144,176 @@
     var
         $win = $(window),
         scrollEvSet,
-        defaultRanges =   50,// px
-        defaultThrottle = 50,// ms
         globalCfg = $.whenOnScreen = {
-            // leftright:  false,
-            range:      defaultRanges,
-            throttle:   defaultThrottle
+            live:       false,
+            leftright:  false,
+            range:      [{ radius:50 }],
+            throttle:   50,
+          },
+
+        _percStrToFunc = function (rangeStr, side) {
+            var m = /^(-?\d+(?:\.\d+)?)%(s|e)$/.exec(rangeStr);
+            var perc = m  &&  parseFloat(m[1]) / 100;
+            var refDim = m  &&  (m[2]==='s' ? 'scr' : 'elm') + (/^[TB]/.test(side) ? 'Height' : 'Width');
+            return m ?
+                      function (sizes/*, side*/) { return perc * sizes[refDim]; }:
+                      parseInt(rangeStr, 10) || 0;
+          },
+
+        _getRangeValue = function (range, rangeFn, sizes, side) {
+            var cacheKey = '_'+side;
+            return !sizes.live && (range[cacheKey]!=null) ?
+                      range[cacheKey]: // return cached value
+                      (range[cacheKey] = rangeFn(sizes, side)); // set cache to calculated value
           },
 
         elements = [],
         elementDatas = [],
-        checkElements = function (elmsToCheckDatas) {
+        lastScrTop,
+        lastScrLeft,
+
+        checkElements = function (elmDatasToCheck) {
             var scrTop = $win.scrollTop(),
-                scrBottom = scrTop + $win.height(),
+                scrHeight = $win.height(),
+                scrBottom = scrTop + scrHeight,
                 scrLeft,  // undefined until at least one element requires it.
+                scrWidth,
                 scrRight; // undefined until at least one element requires it.
-            elmsToCheckDatas = elmsToCheckDatas.push ? elmsToCheckDatas : elementDatas;
-            for (var i=0, data; (data = elmsToCheckDatas[i]); i++)
+            elmDatasToCheck = elmDatasToCheck.push ? // check for .push elmDatasToCheck might be an event object
+                                  elmDatasToCheck:
+                                  elementDatas;
+            for (var i=0, data; (data = elmDatasToCheck[i]); i++)
             {
               var elm = data.elm,
-                  offs = data.live ? elm.offset() : null,
-                  elmTop = offs ? offs.top : data.elmTop,
-                  elmBottom = offs ? elmTop+elm.outerHeight() : data.elmBottom,
+                  live = data.live,
+                  offs = live && elm.offset(),
+                  elmTop =    live ? offs.top : data.elmTop,
+                  elmHeight = live ? elm.outerHeight() : data.elmHeight,
+                  elmBottom = live ? elmTop + elmHeight : data.elmBottom,
                   elmLeft,
+                  elmWidth,
                   elmRight;
               if ( data.leftright )
               {
-                scrLeft  = scrRight ? scrLeft : $win.scrollLeft();
-                scrRight = scrRight || (scrLeft + $win.width());
-                elmLeft  = offs ? offs.left : data.elmLeft;
-                elmRight = offs ? elmLeft+elm.outerWidth() : data.elmRight;
+                if ( !i )
+                {
+                  scrLeft  = $win.scrollLeft();
+                  scrWidth  = $win.width();
+                  scrRight = scrLeft + scrWidth;
+                }
+                elmLeft  = live ? offs.left : data.elmLeft;
+                elmWidth = live ? elm.outerWidth() : data.elmWidth;
+                elmRight = live ? elmLeft + elmWidth : data.elmRight;
               }
-              for (var j=0, range; (range = data.ranges[j]); j++)
+
+              var ev = {
+                      scrTop:    scrTop,
+                      scrHeight: scrHeight,
+                      scrBottom: scrBottom,
+                      scrLeft:   scrLeft,   // undefined unless data.leftright
+                      scrWidth:  scrWidth,  // undefined unless data.leftright
+                      scrRight:  scrRight,  // undefined unless data.leftright
+
+                      elmTop:    elmTop,
+                      elmHeight: elmHeight,
+                      elmBottom: elmBottom,
+                      elmLeft:   elmLeft,   // undefined unless data.leftright
+                      elmWidth:  elmWidth,  // undefined unless data.leftright
+                      elmRight:  elmRight,  // undefined unless data.leftright
+
+                      //isElmBelow: Boolean    // set below
+                      //isElmAbove: Boolean    // set below
+                      //isElmRight: Boolean    // set below
+                      //isElmLeft:  Boolean    // set below
+
+                      lastScrTop:  lastScrTop,  // undefined during first run
+                      lastScrLeft: lastScrLeft, // undefined during first run
+
+                      leftright: data.leftright,
+                      live:      data.live
+                    },
+                  j = 0,
+                  range;
+
+              while ( (range = data.ranges[j++]) )
               {
-                var onScreen = elmTop-range.top < scrBottom  &&  scrTop < elmBottom+range.bottom;
+                var onScreen,
+                    rTop = range.top,
+                    rBottom = range.bottom;
+
+                rTop.call && (rTop = _getRangeValue(range, rTop, ev, 'top'));
+                rBottom.call && (rBottom = _getRangeValue(range, rBottom, ev,'bottom'));
+
+                ev.isElmBelow = elmTop-rTop >= scrBottom;
+                ev.isElmAbove = scrTop >= elmBottom+rBottom;
+                onScreen =  !ev.isElmBelow && !ev.isElmAbove;
+
+                if ( data.leftright )
+                {
+                  var rLeft = range.left,
+                      rRight = range.right;
+
+                  rLeft.call && (rLeft = _getRangeValue(range, rLeft, ev, 'left'));
+                  rRight.call && (rRight = _getRangeValue(range, rRight, ev,'right'));
+
+                  ev.isElmRight =  elmLeft-rLeft >= scrRight;
+                  ev.isElmLeft = scrLeft >= elmRight+rRight;
+                  onScreen =  onScreen && !ev.isElmRight && !ev.isElmLeft;
+                }
+
                 if ( onScreen !== range.onscreen )
                 {
                   range.onscreen = onScreen;
                   // trigger wheno[n|ff]screen event
-                  elm.trigger({
-                      type:      onScreen ? 'whenonscreen' : 'whenoffscreen',
-                      range:     range,
-
-                      scrTop:    scrTop,
-                      scrBottom: scrBottom,
-                      scrLeft:   scrLeft,   // undefined if !data.leftright
-                      scrRight:  scrRight,  // undefined if !data.leftright
-
-                      elmTop:    elmTop,
-                      elmBottom: elmBottom,
-                      elmLeft:   elmLeft,   // undefined if !data.leftright
-                      elmRight:  elmRight,  // undefined if !data.leftright
-
-                      leftright: data.leftright,
-                      live:      data.live
-                    });
+                  ev.type = onScreen ? 'whenonscreen' : 'whenoffscreen';
+                  ev.range = range;
+                  elm.trigger(ev);
                 }
               }
+              lastScrTop = scrTop;
+              lastScrTop = scrTop;
+
             }
 
           };
 
-    $.fn.whenOnScreen = function (opts) {
+        // _reUnit = /\d(%|vw|wh)$/;
+
+
+    $.fn.whenOnScreen = function (opts, arg2) {
           var method = 'run', // run
               ranges;
 
           if ( typeof opts === 'string' )
           {
             method = opts;
-            opts = arguments[1];
+            opts = arg2;
           }
           opts = $.extend({
                     // live:   false,
                     // ranges: $.whenOnScreen.range
                   }, opts);
 
-          if ( method === 'run')
+          if ( method === 'run' )
           {
             // Normalize the options and apply defaults.
             // We can safely skip this if method is 'stop' or 'recalc'
             ranges =  opts.ranges!=null ?
                           opts.ranges:
-                      globalCfg.ranges!=null ?
-                          globalCfg.ranges:
-                          defaultRanges;
+                          globalCfg.ranges;
 
-            ranges =  (typeof ranges === 'number') ?
-                          [{ radius: ranges }]:
-                          ranges;
+            ranges =  $.isArray( ranges ) ?
+                          ranges:
+                          [{ radius: ranges }];
 
             for (var i=0, range; (range = ranges[i]); i++)
             {
               var radius = range.radius || 0;
-              range.top =    range.top!=null ?    range.top    : radius;
-              range.bottom = range.bottom!=null ? range.bottom : radius;
-              range.left =   range.left!=null ?   range.left   : radius;
-              range.right =  range.right!=null ?  range.right  : radius;
+              if ( range.top==null )    { range.top =    radius; }
+              if ( range.bottom==null ) { range.bottom = radius; }
+              if ( range.left==null )   { range.left =   radius; }
+              if ( range.right==null )  { range.right =  radius; }
+              delete range.radius;
             }
           }
 
@@ -218,48 +321,63 @@
           this.each(function () {
               var elm = this,
                   idx = $.inArray(elm, elements),
-                  elmExisted = idx > -1;
+                  alreadyMonitored = idx > -1;
 
               if ( method === 'stop' )
               {
                 // remove the current element data from the list of monitored objects
                 // and do nothing else
-                if ( elmExisted )
+                if ( alreadyMonitored )
                 {
                   elements.splice(idx,1);
                   elementDatas.splice(idx,1);
                 }
               }
-              else
+              else // /^(run|data|recalc)$/.test(method)
               {
-
                 var data = {};
-                if ( elmExisted )
+                if ( alreadyMonitored )
                 {
                   data = elementDatas[idx];
                 }
                 if ( method === 'data' )
                 {
-                  retValue = elmExisted ? data : undefined; // make the plugin return the data of the first item
+                  retValue = alreadyMonitored ? data : undefined; // make the plugin return the data of the first item
                   return false;    // immediately exit this.each() loop
                 }
                 if ( method !== 'recalc' )
                 {
                   // Initialize and normalize (or simply update!) the data object for the current element
                   // (No need to do this if we're just recalculating the dimenstions and posision)
-                  if ( !elmExisted ) {
+                  if ( !alreadyMonitored )
+                  {
                     elements.push(elm);
                     elementDatas.push(data);
                     data.elm = $(elm);
                   }
-                  data.leftright = !!opts.leftright || !!globalCfg.leftright;
-                  data.live =      !!opts.live;
+                  data.leftright = opts.leftright!=null ?
+                                      !!opts.leftright:
+                                      !!globalCfg.leftright;
+                  data.live = opts.live!=null ?
+                                  !!opts.live:
+                                  !!globalCfg.live;
                   data.ranges = [];
                   $.each(ranges, function () {
-                     data.ranges.push( $.extend({}, this) );
+                      data.ranges.push( $.extend({}, this) );
                     });
                 }
-                if ( !opts.live && (!elmExisted || method==='recalc') )
+                // check to see if ranges contain (String) percentage tokens
+                // that need to be converted into a function...
+                // (also check during 'recalc' as the ranges might have been updated)
+                $.each(data.ranges, function (i, range) {
+                    $.each(['Top','Bottom','Left','Right'], function (i, side) {
+                        var sideLC = side.toLowerCase(),
+                            rangeVal = range[sideLC];
+                        range[sideLC] = rangeVal.charAt ? _percStrToFunc(rangeVal, side): rangeVal;
+                        delete range['_'+sideLC]; // purge cached values
+                      });
+                  });
+                if ( !opts.live && (!alreadyMonitored || method==='recalc') )
                 {
                   // Measure the element's current dimensions and position
                   // Skip this if opts.live (because then it happens inside checkElements())
@@ -267,11 +385,13 @@
                   var dataElm = data.elm,
                       offs = dataElm.offset();
                   data.elmTop =    offs.top;
-                  data.elmBottom = offs.top + dataElm.outerHeight();
+                  data.elmHeight = dataElm.outerHeight();
+                  data.elmBottom = offs.top + data.elmHeight;
                   if ( data.leftright )
                   {
                     data.elmLeft =  offs.left;
-                    data.elmRight = offs.left + dataElm.outerWidth();
+                    data.elmWidth = dataElm.outerWidth();
+                    data.elmRight = offs.left + data.elmWidth;
                   }
                 }
                 if ( method === 'run' || !data.live )
@@ -286,17 +406,18 @@
 
           if ( !elements.length )
           {
+            // Monitoring of all elements has been `stopp`ed.  Unbind scroll/resize events.
             $win.off('.whenOnScreen');
             scrollEvSet = false;
           }
           else if ( !scrollEvSet )
           {
+            scrollEvSet = true;
             $win.on('scroll.whenOnScreen resize.whenOnScreen',
                 $.throttleFn && globalCfg.throttle ?
                     $.throttleFn(checkElements, true, globalCfg.throttle):
                     checkElements
               );
-            scrollEvSet = true;
           }
 
           return retValue;
